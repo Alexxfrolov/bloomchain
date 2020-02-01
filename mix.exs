978dfb02cd562c27814d253641968ad30d265c8f
Alexey Frolov <alexeyfrolov@Alexeys-MacBook-Pro.local>
2020-02-01 18:50:40 +0300

    downgrade elxir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Bloomchain.Mixfile do
     [
       app: :bloomchain,
       version: "0.0.1",
-      elixir: "~> 1.10.0",
+      elixir: "~> 1.5.0",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
