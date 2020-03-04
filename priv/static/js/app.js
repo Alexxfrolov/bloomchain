57679cdc135fc723041d0ea4bd45d60b4e123482
Aleksander Moiseenko <oksimilk.developer@gmail.com>
2020-03-04 18:03:17 +0300

    feat:(lib/bloomchain_web/client): add current converter

--- priv/static/js/app.js
+++ priv/static/js/app.js
@@ -1300,13 +1300,16 @@ $('.js-marquee').marquee({
   duplicated: !0
 });
 
-$('body').delegate('.js-scroll-button', 'click', function(event) {
-  const { scroll, date } = event.currentTarget.dataset
+$('body').delegate('.js-scroll-button', 'click', function (event) {
+  const {
+    scroll,
+    date
+  } = event.currentTarget.dataset
 
   if (!!scroll) {
     const path = `${location.pathname}?scroll=${scroll}&last_date=${date}`
 
-    $.get(path, function(response) {
+    $.get(path, function (response) {
       const $main = $('.js-main')
       const $button = $('.container.px-0.pb-5')
       html = $.parseHTML(response)
@@ -1315,3 +1318,26 @@ $('body').delegate('.js-scroll-button', 'click', function(event) {
     })
   }
 })
+
+const $currencyConverterInputField = $('.js-currency-converter-input')
+const $currencyConverterOutputField = $('.js-currency-converter-output')
+const $cryptoCurrencySelect = $('.js-crypto-currency-select')
+const $financeCurrencySelect = $('.js-finance-currency-select')
+
+$currencyConverterInputField.ready(function () {
+  setCurrency($currencyConverterInputField.val())
+})
+
+$currencyConverterInputField.on('blur', function (event) {
+  setCurrency(event.target.value)
+})
+
+function setCurrency(cryptoValue) {
+  const fsym = $cryptoCurrencySelect.val()
+  const tsyms = $financeCurrencySelect.val()
+  $.get(`https://min-api.cryptocompare.com/data/price?fsym=${fsym}&tsyms=${tsyms}`, function (data) {
+    const currency = data[tsyms]
+    const value = (cryptoValue * currency).toFixed(2)
+    $currencyConverterOutputField.text(value)
+  })
+}
