30de2101a613533e753d3e85407621aaa72a281c
Alexxfrolov <alex99-999@yandex.ru>
2020-03-16 15:47:49 +0300

    add fuzzines to es

--- lib/bloomchain/elasticsearch/cluster.ex
+++ lib/bloomchain/elasticsearch/cluster.ex
@@ -33,7 +33,8 @@ defmodule Bloomchain.ElasticsearchCluster do
                 query: query,
                 fields: ["title^3", "lead^2", "body"],
                 tie_breaker: 0.1,
-                minimum_should_match: "75%"
+                type: "most_fields",
+                fuzziness: "auto"
               }
             }
           ],
