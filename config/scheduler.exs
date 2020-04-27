5d85f02c05aec82411a2532587de52b3a64e59a6
Alexey Frolov <alexeyfrolov@Alexeys-MacBook-Pro.local>
2020-04-27 23:31:53 +0300

    correct sheduler

--- config/scheduler.exs
+++ config/scheduler.exs
@@ -3,7 +3,7 @@ use Mix.Config
 config :bloomchain, Bloomchain.Scheduler,
   jobs: [
     # Every minute
-    {"* * * * *", {Mix.Task, :rerun, ["bloomchain.publish_post"]}}
+    {"* * * * *", {Mix.Task, :rerun, ["bloomchain.publish_post"]}},
     # Every 15 minutes
-    # {"*/15 * * * *", {Heartbeat, :send, []}}
+    {"*/15 * * * *", {Mix.Task, :rerun, ["bloomchain.index", [Bloomchain.Index.Bitcoin]]}}
   ]
