dae57a0fb053b945472de0788ef070fce2d79ba0
Alexey Frolov <alexeyfrolov@Alexeys-MacBook-Pro.local>
2020-05-05 15:49:36 +0300

    sitemaps for posts

--- lib/bloomchain_web/controllers/sitemap_controller.ex
+++ lib/bloomchain_web/controllers/sitemap_controller.ex
@@ -40,4 +40,26 @@ defmodule BloomchainWeb.SitemapController do
     conn
     |> render("tags.xml", tags: tags)
   end
+
+  def show(conn, %{property: property}) do
+    [year | [month, _]] = String.split(property, ["posts-", "-", ".xml"]) |> tl()
+
+    year = String.to_integer(year)
+    month = String.to_integer(month)
+
+    date_start = Timex.to_datetime({year, month, 1})
+    date_end = Timex.to_datetime({year, month, :calendar.last_day_of_the_month(year, month)})
+
+    posts =
+      from(p in Post,
+        select: %{slug: p.slug, type: p.type, updated_at: p.updated_at},
+        where:
+          p.status == "published" and p.published_at >= ^date_start and p.published_at < ^date_end,
+        limit: 10_000
+      )
+      |> Repo.all()
+
+    conn
+    |> render("posts.xml", posts: posts)
+  end
 end
