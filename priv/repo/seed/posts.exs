74f5a995dc409b6e46b9076a1a1acedc01b5d0b8
Alexey Frolov <alexeyfrolov@Alexeys-MacBook-Pro.local>
2020-05-09 13:33:49 +0300

    replace spans, twitter, youtube

--- priv/repo/seed/posts.exs
+++ priv/repo/seed/posts.exs
@@ -1,9 +1,35 @@
 alias Bloomchain.Repo
 alias Bloomchain.Content.{Post, Article}
 
+replace_span = fn item ->
+  item
+  |> String.replace("<blockquote><span style=\"font-weight: 400;\">", "<blockquote><p>")
+  |> String.replace("</span></blockquote>", "</p></blockquot")
+end
+
+replace_embedly_urls = fn item ->
+  Regex.replace(
+    ~r/(https:\/\/twitter.com\/.*s=20)|(https:\/\/www.youtube.com\/watch\?v=[a-zA-Z0-9]*)/,
+    item,
+    fn url, _ ->
+      "<div class=\"fr-embedly\" data-original-embed=\"<a href='#{url}\" data-card-branding=\"0\" class=\"embedly-card\"></a><a href=\"#{
+        url
+      }\" data-card-branding=\"0\" class=\"embedly-card\"></a></div>"
+    end
+  )
+end
+
 "#{File.cwd!()}/priv/repo/data_files/posts.json"
 |> File.read!()
 |> Poison.decode!(keys: :atoms)
+|> Enum.map(fn item ->
+  body =
+    item.body
+    |> replace_span.()
+    |> replace_embedly_urls.()
+
+  Map.replace!(item, :body, body)
+end)
 |> Enum.each(fn item ->
   Repo.insert!(
     Post.changeset(%Post{}, %{
