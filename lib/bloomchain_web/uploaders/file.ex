c29f727b5028a7836c638fbfac71453c2bcac96c
Alexey Frolov <alexeyfrolov@Alexeys-MacBook-Pro.local>
2020-05-27 21:37:14 +0300

    make s3 public

--- lib/bloomchain_web/uploaders/file.ex
+++ lib/bloomchain_web/uploaders/file.ex
@@ -3,6 +3,7 @@ defmodule BloomchainWeb.Uploaders.File do
   use Arc.Ecto.Definition
 
   # @versions [:original]
+  @acl :public_read
 
   def validate({file, %{type: "image"}}) do
     ~w(.jpg .jpeg .png) |> Enum.member?(Path.extname(file.file_name))
