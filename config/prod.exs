c8ff08138b96c72a208437d5b7975a23220f447b
Nate Tuganov <nt@clain.io>
2020-06-11 22:46:43 +0000

    sentry config

--- config/prod.exs
+++ config/prod.exs
@@ -52,4 +52,14 @@ config :bloomchain, Bloomchain.ElasticsearchCluster,
   username: System.get_env("ES_USER"),
   password: System.get_env("ES_PASSWORD")
 
+config :sentry,
+  dsn: "https://4ceb6862d2b6458fa32dd6f4644b57fc@o252637.ingest.sentry.io/5273561",
+  environment_name: :prod,
+  enable_source_code_context: true,
+  root_source_code_path: File.cwd!,
+  tags: %{
+    env: "production"
+  },
+  included_environments: [:prod]
+  
 import_config "scheduler.exs"
