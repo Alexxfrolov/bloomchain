d574a9d6d7e4c2a5b1ea4806f30cb9a9f487a09f
Alexey Frolov <alexeyfrolov@Alexeys-MacBook-Pro.local>
2020-06-16 13:15:45 +0300

    uncomment scheduler

--- config/scheduler.exs
+++ config/scheduler.exs
@@ -3,15 +3,19 @@ use Mix.Config
 config :bloomchain, Bloomchain.Scheduler,
   jobs: [
     # Every minute
-    {"* * * * *", {Mix.Task, :rerun, ["bloomchain.publish_post"]}}
+    {"* * * * *", {Mix.Task, :rerun, ["bloomchain.publish_post"]}},
     # # Every 15 minutes
-    # {"*/15 * * * *",
-    #  fn ->
-    #    Mix.Task.rerun("bloomchain.update_coin_price")
-    #    Mix.Task.rerun("bloomchain.update_index", [Bloomchain.Service.Index.Top10])
-    #  end},
-    # {"*/15 * * * *",
-    #  {Mix.Task, :rerun, ["bloomchain.update_index", [Bloomchain.Service.Index.Bitcoin]]}},
+    {"*/15 * * * *",
+     fn ->
+       # При первом запуске обязательно надо иметь 2 значание монеты из-за неверной логики в алгоритме
+       # иначе график улетает в космос так как биткоин и все топ 10 монет считаются новыми
+       # неужно менять логику для новых монет
+       Mix.Task.run("bloomchain.update_coin_price")
+       Mix.Task.rerun("bloomchain.update_coin_price")
+       Mix.Task.rerun("bloomchain.update_index", [Bloomchain.Service.Index.Top10])
+     end},
+    {"*/15 * * * *",
+     {Mix.Task, :rerun, ["bloomchain.update_index", [Bloomchain.Service.Index.Bitcoin]]}},
     # # Every day at 00:00 UTC
-    # {"0 0 * * *", {Mix.Task, :rerun, ["bloomchain.update_coin_list"]}}
+    {"0 0 * * *", {Mix.Task, :rerun, ["bloomchain.update_coin_list"]}}
   ]
