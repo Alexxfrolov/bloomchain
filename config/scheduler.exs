ff1b0bad6928bc8cca292e6f5647a6921e660661
Alexey Frolov <alexeyfrolov@Alexeys-MacBook-Pro.local>
2020-06-16 14:29:20 +0300

    change scheduler

--- config/scheduler.exs
+++ config/scheduler.exs
@@ -5,14 +5,15 @@ config :bloomchain, Bloomchain.Scheduler,
     # Every minute
     {"* * * * *", {Mix.Task, :rerun, ["bloomchain.publish_post"]}},
     # # Every 15 minutes
-    {"*/15 * * * *",
+    {"*/2 * * * *",
      fn ->
        # При первом запуске обязательно надо иметь 2 значание монеты из-за неверной логики в алгоритме
        # иначе график улетает в космос так как биткоин и все топ 10 монет считаются новыми
-       # неужно менять логику для новых монет
-       Mix.Task.run("bloomchain.update_coin_price")
+       # нужно менять логику для новых монет
+
        Mix.Task.rerun("bloomchain.update_coin_price")
-       Mix.Task.rerun("bloomchain.update_index", [Bloomchain.Service.Index.Top10])
+       Mix.Task.rerun("bloomchain.update_coin_price")
+       # Mix.Task.rerun("bloomchain.update_index", [Bloomchain.Service.Index.Top10])
      end},
     {"*/15 * * * *",
      {Mix.Task, :rerun, ["bloomchain.update_index", [Bloomchain.Service.Index.Bitcoin]]}},
