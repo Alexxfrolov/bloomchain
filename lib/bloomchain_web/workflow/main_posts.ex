698f2418670c861a3672113e29afba8a7a7c22f0
Alex Frolov <alex99-999@yandex.ru>
2020-11-23 23:21:45 +0300

    chore: increase total number on articles on main page

--- lib/bloomchain_web/workflow/main_posts.ex
+++ lib/bloomchain_web/workflow/main_posts.ex
@@ -4,13 +4,13 @@ defmodule BloomchainWeb.Workflow.MainPosts do
 
   def run() do
     [
-      detailed: Article.published_posts("detailed") |> Repo.limit(3),
-      newsfeed: Article.published_posts("newsfeed") |> Repo.limit(7),
-      analysis: Article.published_posts("analysis") |> Repo.limit(3),
-      people: Article.published_posts("people") |> Repo.limit(3),
-      # in_russia: Article.published_posts("in-russia") |> Repo.limit(3),
-      calendar: Article.published_posts("calendar") |> Repo.limit(3),
-      research: Article.published_posts("research") |> Repo.limit(3)
+      detailed: Article.published_posts("detailed") |> Repo.limit(7),
+      newsfeed: Article.published_posts("newsfeed") |> Repo.limit(14),
+      analysis: Article.published_posts("analysis") |> Repo.limit(7),
+      people: Article.published_posts("people") |> Repo.limit(7),
+      # in_russia: Article.published_posts("in-russia") |> Repo.limit(7),
+      calendar: Article.published_posts("calendar") |> Repo.limit(7),
+      research: Article.published_posts("research") |> Repo.limit(7)
     ]
   end
 end
